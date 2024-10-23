852f6b9a0bdad35267595aae840ef81fb3e44f77
abhilashasupe <abhilasha.supe@gmail.com>
2024-10-23 12:31:08 +0530

    frontend for rule insertion

--- src/main.js
+++ src/main.js
@@ -1,52 +1,17 @@
 import express from 'express'; 
 import connectDB from './config/connect.js'; 
-// import { insertAstToDB, evaluateRule , combineRules } from './controllers/astController.js'; 
-import router from './routes/astRoutes.js'
-import dotenv from 'dotenv';
-dotenv.config(); 
+
 const app = express();
 app.use(express.json());
 
+import cors from 'cors'; 
+app.use(cors());
+
+import router from './routes/astRoutes.js';
 app.use("/" , router)
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World'); 
-// });
-
-// app.post('/insert-rule', async (req, res) => {
-//   const { rule, ruleName } = req.body;
-//   try {
-//     await insertAstToDB(req.body); 
-//     res.status(200).json({ message: `AST for rule "${ruleName}" inserted successfully` });
-//   } catch (error) {
-//     // console.error('Error inserting AST:', error);
-//     res.status(500).json({ message: 'Error inserting AST', error: error.message });
-//   }
-// });
-
-// app.post('/evaluate-rule', async (req, res) => {
-//     const { userData, ruleName } = req.body;
-//     try{
-//         const { ast, isEligible}  = await evaluateRule(ruleName, userData); 
-//         // console.log("Fetched AST:", ast);
-//         // console.log("This data is eligible if true:", isEligible);
-//         res.status(200).json({ message: ` is eligible : ` , isEligible });
-//       }catch(error) {
-//           res.status(500).json({ message: 'Error fetching rule ', error: error.message });
-//       }
-// });
-
-
-// app.post('/combine-rules', async (req, res) => {
-//   const { ruleName1, ruleName2, operator, newRuleName } = req.body;
-//   try{
-//       await combineRules(ruleName1, ruleName2, operator, newRuleName); 
-//       res.status(200).json({ message: ` rules combined successfully ` });
-//     }catch(error) {
-//         res.status(500).json({ message: 'Error combining rules ', error: error.message });
-//     }
-// });
- 
+import dotenv from 'dotenv';
+dotenv.config();
 
 const PORT = 3333;
 const run = async()=>{
